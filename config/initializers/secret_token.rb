89d229918b24141d86017b58ebe411b5c5a86c9d
EC2 Default User <ec2-user@ip-172-31-18-28.ap-northeast-1.compute.internal>
2013-06-27 10:02:24 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot2::Application.config.secret_token = '12374efe0a0ce93edc1eacb3796b2d59374103f07123cdef1c0a8cec6e1cb2dccedc0d25524d07c8318c2e80342954673407625fb6d0d51a28fe1bd0765102e9'
